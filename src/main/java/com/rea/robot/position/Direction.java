de7659946336fc133533ddfee6fafd48dd461e77
Ramanjit Singh <rsingh@aconex.com>
2017-12-07 17:50:08 +1100

    ToyRobot : Direction enum to provide implementation of move based on current location and direction

--- src/main/java/com/rea/robot/position/Direction.java
+++ src/main/java/com/rea/robot/position/Direction.java
@@ -1,8 +1,35 @@
 package com.rea.robot.position;
 
 public enum Direction {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST
+    NORTH {
+        public void move(RobotLocation currentLocation) {
+            if(currentLocation.isLocationValid(currentLocation.getRobotTableRow(), currentLocation.getRobotTableColumn() + 1)) {
+                currentLocation.setRobotTableColumn(currentLocation.getRobotTableColumn() + 1);
+            }
+        }
+    },
+    SOUTH {
+        public void move(RobotLocation currentLocation) {
+            if(currentLocation.isLocationValid(currentLocation.getRobotTableRow(), currentLocation.getRobotTableColumn() - 1)) {
+                currentLocation.setRobotTableColumn(currentLocation.getRobotTableColumn() - 1);
+            }
+        }
+    },
+    WEST {
+        public void move(RobotLocation currentLocation) {
+            if(currentLocation.isLocationValid(currentLocation.getRobotTableRow() -1, currentLocation.getRobotTableColumn())) {
+                currentLocation.setRobotTableRow(currentLocation.getRobotTableRow() - 1);
+            }
+        }
+    },
+    EAST {
+        public void move(RobotLocation currentLocation) {
+            if(currentLocation.isLocationValid(currentLocation.getRobotTableRow() + 1, currentLocation.getRobotTableColumn())) {
+                currentLocation.setRobotTableRow(currentLocation.getRobotTableColumn() + 1);
+            }
+        }
+    };
+
+    public abstract void move(RobotLocation currentLocation);
+
 }
