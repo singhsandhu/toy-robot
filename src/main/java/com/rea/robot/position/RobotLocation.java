14dda4b0edf5fb575a3b5dad4aae17ec8c25752e
Ramanjit Singh <rsingh@aconex.com>
2017-12-07 17:48:30 +1100

    ToyRobot : RobotLocation class will represent current location (x,y) of the table in the table

--- src/main/java/com/rea/robot/position/RobotLocation.java
+++ src/main/java/com/rea/robot/position/RobotLocation.java
@@ -2,7 +2,31 @@ package com.rea.robot.position;
 
 public class RobotLocation {
 
-    public static final int X_ROWS = 5;
-    public static final int Y_ROWS = 5;
+    public static final int TABLE_ROWS_LIMIT = 5;
+    public static final int TABLE_COLUMN_LIMIT = 5;
+
+    private int robotTableRow;
+    private int robotTableColumn;
+
+    public int getRobotTableRow() {
+        return robotTableRow;
+    }
+
+    public void setRobotTableRow(int robotTableRow) {
+        this.robotTableRow = robotTableRow;
+    }
+
+    public int getRobotTableColumn() {
+        return robotTableColumn;
+    }
+
+    public void setRobotTableColumn(int robotTableColumn) {
+        this.robotTableColumn = robotTableColumn;
+    }
+
+    public boolean isLocationValid(int row, int column) {
+        return row >=0 && row < TABLE_ROWS_LIMIT
+                && column >=0 && column <TABLE_COLUMN_LIMIT;
+    }
 
 }
